a4cd99bb3ea9f8cf2e3a6810087390636cfe8509
roopsai <roopsai@outlook.in>
2025-01-06 22:01:18 +0530

    Add Retro Application Project

---
+++ java/retro/src/main/java/in/roopsai/retro/RetroApplication.java
@@ -0,0 +1,13 @@
+package in.roopsai.retro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetroApplication.class, args);
+	}
+
+}
---
+++ java/retro/src/test/java/in/roopsai/retro/RetroApplicationTests.java
@@ -0,0 +1,13 @@
+package in.roopsai.retro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
